1408daa93e1524be9c5865ce3d19b2f0954950b8
Maria Rodriguez <marialerodrio@gmail.com>
2023-08-04 14:28:19 -0400

    Primer commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,41 @@
+import './mylabel.css';
+
+interface MyLabelProps {
+    /**
+   * Este mensaje es el que se va a usar en la etiqueta
+   */
+    label: string;
+    /**
+   * Este es el tamaño por defecto del label
+   */
+    size: 'normal'|'h1'|'h2'|'h3';
+    /**
+     * Capitalizar toda la palabra
+     */
+    allCaps?: boolean;
+    /**
+     * Colores del basicos del boton
+     */
+    color?: 'primary' | 'secundary' | 'tertiary';
+    /**
+     * Color personalizado de la fuente
+     */
+    fontColor?: string;
+}
+
+export const MyLabel = ({
+    label = 'No label',
+    size = 'normal',
+    color = 'primary',
+    allCaps = false,
+    fontColor 
+}: MyLabelProps ) => {
+  return (
+    <span 
+    className={` label ${ size } text-${ color }`}
+    style={{ color: fontColor }}>
+    { allCaps ? label.toUpperCase() : label }
+    </span>
+  )
+}
+
